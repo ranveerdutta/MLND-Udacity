6a5e9ba6f28b75134575467a2148cf9f05407ff9
Ranveer Dutta <ranveer.dutta@gmail.com>
2017-08-12 13:51:34 +0530

    Update SpamDetection.py
    removing duplicate lines

--- Spam Detection/SpamDetection.py
+++ Spam Detection/SpamDetection.py
@@ -33,15 +33,6 @@ print('Number of rows in the total set: {}'.format(df.shape[0]))
 print('Number of rows in the training set: {}'.format(X_train.shape[0]))
 print('Number of rows in the test set: {}'.format(X_test.shape[0]))
 
-# Instantiate the CountVectorizer method
-count_vector = CountVectorizer()
-
-# Fit the training data and then return the matrix
-training_data = count_vector.fit_transform(X_train)
-
-# Transform testing data and return the matrix. Note we are not fitting the testing data into the CountVectorizer()
-testing_data = count_vector.transform(X_test)
-
 
 
 
